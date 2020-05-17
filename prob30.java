6af9be496b50dd3efb110f6b108fec2f136b8ee7
Samuel Wood-Soloff <samuelw2022@headroyce.org>
2020-05-17 13:02:57 -0700

    prob 30

--- prob30.java
+++ prob30.java
@@ -1,95 +1,285 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class prob30 {
-	static char[][] grid;
-	static char[][] gridClone; // unused rn
-	static int N;
-	public static void main(String[] args) throws IOException {
-		BufferedReader f = new BufferedReader(new FileReader("input.txt"));
-		int N = Integer.parseInt(f.readLine());
-		grid = new char[N][N];
-		for (int i = 0; i < N; i++) {
-			grid[i] = f.readLine().toCharArray();
-		}
-		gridClone = grid.clone(); // unused rn
-//		for (int i = 0; i < N; i++) {
-//			System.out.println(Arrays.toString(grid[i]));
-//		} // PASS
-		fill2x2();
-		// pass top, right, bottom, left (outer --> inner)
-		for (int i = 0; i < (N+1)/2; i++) {
-			box(i, i, i, N-i-1);
-			box(i, i, i, N-i-1);
-		}
-	}
-	
-	static void box(int i1, int i2, int j1, int j2) {
-		String t = "";
-		if (i1==i2) {
-			t = grid[i1].toString().substring(j1,j2+1);
-		} else {
-			for (int c = i1; c <= i2; c++) {
-				t += grid[c][j1];
+	public static void main (String[] args) throws NumberFormatException, IOException {
+		String cake[][];
+		BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+		int n = Integer.parseInt(br.readLine());
+		String tmp = "";
+		String[] tmparr = new String[n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				
 			}
 		}
-		int fd = -1;
-		int fh = -1;
-		if (t.contains(".")) {
-			fd = t.indexOf('.');
-		}
-		if (t.contains("#")) {
-			fh = t.indexOf('#');
-		}
+		Node root = new Node(cake);
+		
 	}
 	
-	static char is2x2(int i, int j) {
-		// top left, top right, bottom right, bottom left
-		Set<Character> types = new HashSet<Character>();
-		types.add('#');
-		types.add('.');
-		for (char t : types) {
-			try {
-				if (grid[i][j-1] == t && grid[i-1][j-1] == t && grid[i-1][j] == t) return t;
-			} catch (Exception e) {}
-			try {
-				if (grid[i][j+1] == t && grid[i-1][j+1] == t && grid[i-1][j] == t) return t;
-			} catch (Exception e) {}
-			try {
-				if (grid[i][j+1] == t && grid[i+1][j+1] == t && grid[i+1][j] == t) return t;
-			} catch (Exception e) {}
-			try {
-				if (grid[i][j-1] == t && grid[i+1][j-1] == t && grid[i+1][j] == t) return t;
-			} catch (Exception e) {}
-		}
-		return 'c'; // not 2x2
-	}
-	
-	static void fill2x2() {
-		int count;
-		do {
-			count = 0;
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					char t = is2x2(i,j);
-					if (t != 'c') { 
-						grid[i][j] = t == '#' ? '.' : '#';
-						count++;
-					}
+	private static String[][] fillInGivens(String[][] arr) {
+		String[][] answer = arr;
+		
+		for (int i = 1; i < arr.length - 1; i++) {
+			for (int j = 1; j < arr.length - 1; j++) {
+				//change to deal with corners, tops, sides, bottoms, and all 4 2x2 squares
+				if (arr[i][j].equals("?") && arr[i][j - 1].equals(".") && arr[i - 1][j].equals(".") && arr[i - 1][j - 1].equals(".")) {
+					arr[i][j] = "#";
+				}
+				
+				if (arr[i][j].equals("?") && arr[i][j + 1].equals(".") && arr[i + 1][j].equals(".") && arr[i + 1][j + 1].equals(".")) {
+					arr[i][j] = "#";
+				}
+				
+				if (arr[i][j].equals("?") && arr[i][j - 1].equals("#") && arr[i - 1][j].equals("#") && arr[i - 1][j - 1].equals("#")) {
+					arr[i][j] = ".";
+				}
+				
+				if (arr[i][j].equals("?") && arr[i][j + 1].equals("#") && arr[i + 1][j].equals("#") && arr[i + 1][j + 1].equals("#")) {
+					arr[i][j] = ".";
+				}
+				
+				if (arr[i][j].equals("?") && arr[i][j - 1].equals("#") && arr[i + 1][j].equals("#") && arr[i + 1][j - 1].equals("#")) {
+					arr[i][j] = ".";
+				}
+				
+				if (arr[i][j].equals("?") && arr[i][j - 1].equals(".") && arr[i + 1][j].equals(".") && arr[i + 1][j - 1].equals(".")) {
+					arr[i][j] = ".";
+				}
+				
+				if (arr[i][j].equals("?") && arr[i][j - 1].equals("#") && arr[i - 1][j].equals("#") && arr[i - 1][j - 1].equals("#")) {
+					arr[i][j] = "#";
+				}
+				
+				if (arr[i][j].equals("?") && arr[i - 1][j].equals(".") && arr[i][j + 1].equals(".") && arr[i - 1][j + 1].equals(".")) {
+					arr[i][j] = "#";
 				}
 			}
-		} while (count != 0);
+		}
+		
+		return answer;
 	}
 	
-	static String stringFill(String str) {
-		int firstDot = -1;
-		int firstHash = -1;
-		if (str.contains(".")) {
-			firstDot = str.indexOf('.');
+	private static boolean connectedCheck(String[][] arr) {
+		int hashes = 0;
+		int dots = 0;
+		int[] dotStartPoint = new int[2];
+		int[] hashStartPoint = new int[2];
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				if (arr[i][j].equals(".")) {
+					dots++;
+					dotStartPoint[0] = i;
+					dotStartPoint[1] = j;
+				}
+				
+				if (arr[i][j].equals("#")) {
+					hashes++;
+					hashStartPoint[0] = i;
+					hashStartPoint[1] = j;
+				}
+			}
 		}
-		if (str.contains("#")) {
-			firstHash = str.indexOf('#');
+		
+		if (expand(arr, new ArrayList<String>(), ".", dotStartPoint) == dots && expand(arr, new ArrayList<String>(), "#", hashStartPoint) == hashes) {
+			return true;
 		}
-		return "";
+		
+		return false;
 	}
 	
+	private static int expand(String[][] arr, ArrayList<String> pointsUsed, String type, int[] point) {
+		int answer = 0;
+		int i = point[0];
+		int j = point[1];
+		if (i - 1 > 0) {
+			if (arr[i - 1][j].equals(type) && pointsUsed.contains((i - 1) + "" + j) == false ) {
+				pointsUsed.add((i - 1) + "" + j);
+				answer++;
+				answer = answer + expand(arr, pointsUsed, type, new int[] {i - 1, j });
+			}
+		}
+		
+		if (j - 1 > 0) {
+			if (arr[i][j - 1].equals(type) && pointsUsed.contains(i + "" + (j - 1)) == false ) {
+				pointsUsed.add(i + "" + (j - 1));
+				answer++;
+				answer = answer + expand(arr, pointsUsed, type, new int[] {i, j - 1});
+			}
+		}
+		
+		if (j + 1 < arr.length) {
+			if (arr[i][j + 1].equals(type) && pointsUsed.contains(i + "" + (j + 1)) == false ) {
+				pointsUsed.add(i + "" + (j + 1));
+				answer++;
+				answer = answer + expand(arr, pointsUsed, type, new int[] {i, j + 1});
+			}
+		}
+		
+		if (i + 1 > 0) {
+			if (arr[i + 1][j].equals(type) && pointsUsed.contains((i + 1) + "" + j) == false ) {
+				pointsUsed.add((i + 1) + "" + j);
+				answer++;
+				answer = answer + expand(arr, pointsUsed, type, new int[] {i + 1, j});
+			}
+		}
+		
+		return answer;
+	}
+	
+	private static boolean loopCheck(String[][] arr) {
+		//checks for 3x3 loops and for an outer loop
+		
+		
+		for (int i = 0; i < arr.length - 1; i++) {
+			if (arr[0][i].equals(arr[0][i + 1])) {
+				return false;
+			}
+		}
+		
+		for (int i = 0; i < arr.length - 1; i++) {
+			if (arr[arr.length - 1][i].equals(arr[arr.length - 1][i + 1])) {
+				return false;
+			}
+		}
+		
+		for (int i = 0; i < arr.length - 1; i++) {
+			if (arr[i][0].equals(arr[i + 1][0])) {
+				return false;
+			}
+		}
+		
+		for (int i = 0; i < arr.length - 1; i++) {
+			if (arr[i][arr.length - 1].equals(arr[i + 1][arr.length - 1])) {
+				return false;
+			}
+		}
+		
+		//checking for 3x3 loops
+		for (int i = 1; i < arr.length - 1; i++) {
+			for (int j = 1; j < arr.length - 1; j++) {
+				if (arr[i-1][j-1].equals("#") && arr[i-1][j].equals("#") && arr[i-1][j+1].equals("#") && arr[i][j-1].equals("#") && arr[i][j+1].equals("#") && arr[i+1][j-1].equals("#") && arr[i+1][j].equals("#") && arr[i+1][j+1].equals("#")) {
+					return false;
+				}
+				
+				if (arr[i-1][j-1].equals(".") && arr[i-1][j].equals(".") && arr[i-1][j+1].equals(".") && arr[i][j-1].equals(".") && arr[i][j+1].equals(".") && arr[i+1][j-1].equals(".") && arr[i+1][j].equals(".") && arr[i+1][j+1].equals(".")) {
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	}
+	
+	//could make more efficient by doing (n - 1)^2
+	private static boolean twoByTwoCheck(String[][] arr) {
+		
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int  j = 0; j < arr.length - 1; j++) {
+				if (arr[i][j].equals("#") && arr[i + 1][j].equals("#") && arr[i][j + 1].equals("#") && arr[i + 1][j + 1].equals("#")) {
+					return false;
+				}
+				
+				if (arr[i][j].equals(".") && arr[i + 1][j].equals(".") && arr[i][j + 1].equals(".") && arr[i + 1][j + 1].equals(".")) {
+					return false;
+				}
+			}
+		}
+		
+		
+		for (int i = arr.length - 1; i > 0; i--) {
+			for (int  j = arr.length - 1; j > 0; j--) {
+				if (arr[i][j].equals("#") && arr[i - 1][j].equals("#") && arr[i][j - 1].equals("#") && arr[i - 1][j - 1].equals("#")) {
+					return false;
+				}
+				
+				if (arr[i][j].equals(".") && arr[i - 1][j].equals(".") && arr[i][j - 1].equals(".") && arr[i - 1][j - 1].equals(".")) {
+					return false;
+				}
+			}
+		}
+		
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int  j = arr.length - 1; j > 0; j--) {
+				if (arr[i][j].equals("#") && arr[i + 1][j].equals("#") && arr[i][j - 1].equals("#") && arr[i + 1][j - 1].equals("#")) {
+					return false;
+				}
+				
+				if (arr[i][j].equals(".") && arr[i + 1][j].equals(".") && arr[i][j - 1].equals(".") && arr[i + 1][j - 1].equals(".")) {
+					return false;
+				}
+			}
+		}
+		
+		for (int i = arr.length - 1; i > 0; i--) {
+			for (int  j = 0; j < arr.length - 1; j++) {
+				if (arr[i][j].equals("#") && arr[i - 1][j].equals("#") && arr[i][j + 1].equals("#") && arr[i - 1][j + 1].equals("#")) {
+					return false;
+				}
+				
+				if (arr[i][j].equals(".") && arr[i - 1][j].equals(".") && arr[i][j + 1].equals(".") && arr[i - 1][j + 1].equals(".")) {
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	}
+	
+	public static int[] findNextQuestionMark(String[][] arr) {
+		int[] answer = new int[2];
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				if (arr[i][j].equals("?")) {
+					answer[0] = i;
+					answer[1] = j;
+					return answer;
+				}
+			}
+		}
+		
+		return null;
+	}
+	
+	class Node {
+		String[][] cake; 
+	
+		public Node (String[][] arr) {
+			cake = arr;
+		}
+		
+		public String[][] iterate(String[][] arr) {
+			int[] tmpLocation = findNextQuestionMark(cake);
+			
+			if (tmpLocation == null) {
+				if (twoByTwoCheck(arr) == true && connectedCheck(arr) == true && loopCheck(arr) == true) {
+					return arr;
+				}
+				
+				return null;
+			}
+			
+			else {
+				String[][] leftBranch = arr;
+				String[][] rightBranch = arr;
+				leftBranch[tmpLocation[0]][tmpLocation[1]] = ".";
+				rightBranch[tmpLocation[0]][tmpLocation[1]] = "#";
+				String[][] left = iterate(leftBranch);
+				String[][] right = iterate(rightBranch);
+				
+				if (left == null & right == null) {
+					return null;
+				}
+				
+				if (left == null & right != null) {
+					return right;
+				}
+				
+				return left;
+			}
+		}
+	}
 }
